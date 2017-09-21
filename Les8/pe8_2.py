84a913e66b7bf009d1a7e9e159728bee04c46e6d
Joostluijben <joost.luijben@student.hu.nl>
2017-09-21 10:59:17 +0200

    Opdracht 8_2

--- Les8/pe8_2.py
+++ Les8/pe8_2.py
@@ -1,3 +1,22 @@
 def monopolyworp():
-    ''
+    import random
+    count = 0
+    x = random.randrange(1, 7)
+    y = random.randrange(1, 7)
+    p = x + y
+    while count < 3:
+        if x == y:
+            x = random.randrange(1, 7)
+            y = random.randrange(1, 7)
+            p = x + y
+            count += 1
+            if count < 3:
+                print('{} + {} = {} (dubbel)'.format(x, y, p))
+            elif count == 3:
+                print('{} + {} = {} (direct naar gevagenis)'.format(x, y, p))
+        else:
+            print('{} + {} = {}'.format(x, y, p))
+            break
+
+
 monopolyworp()
