fe46d3af82629218a34b253a3140320bda39e537
Joostluijben <joost.luijben@student.hu.nl>
2017-11-05 19:07:39 +0100

    Finished lesson 11

--- Les11/autorise.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-auth_details = ('joost.luijben@yahoo.com', 'C1t1p9MIBDuqwi26CRnGfNnN_BZ-OkRjmSeiyf57nW9WmSUd1MWJPQ')
-api_url = 'http://webservices.ns.nl/ns-api-avt?station=ut'
-
-response = requests.get(api_url, auth=auth_details)
-
-with open('departures.xml', 'w') as writeDepartureXml:
-    writeDepartureXml.write(response.text)
--- Les11/reader.py
+++ Les11/reader.py
@@ -1,6 +1,39 @@
 import xmltodict
+import datetime
+import authorise
+import time
+
+authorise.writeXML()
+
 with open('departures.xml') as readDepartureXml:
     stationsdict = xmltodict.parse(readDepartureXml.read())
+def trainDepature():
+    for trains in stationsdict['ActueleVertrekTijden']['VertrekkendeTrein']:
+        try:
+            if (trains['EindBestemming'] == 'Utrecht Centraal') or ('Utrecht C' in trains['RouteTekst']):
+                departure = trains['VertrekTijd'][11:16]
+                timeNow = datetime.datetime.strptime(datetime.datetime.today().strftime('%H:%M'), '%H:%M')
+                departureTime = datetime.datetime.strptime(departure, '%H:%M')
+                timeDifference =  datetime.datetime.strptime(departure, '%H:%M') - timeNow
+                hoursDifference = int(str(timeDifference)[0:1])
+                hoursDifferenceToMinutes = int(hoursDifference * 60)
+                minutesDifference = int(str(timeDifference)[2:4])
+                totalDifference = hoursDifference + minutesDifference
 
-for trainKind in stationsdict['ActueleVertrekTijden']['VertrekkendeTrein']['TreinSoort']:
-    print(trainKind)
+                trainKind = trains['TreinSoort']
+                print('The next train ({}) leaves in {} minutes'.format(trainKind, totalDifference))
+
+                if totalDifference >=15:
+                    print('You can catch this train!\n')
+                elif totalDifference < 15 and totalDifference > 10:
+                    print('You might be able to catch this train if you leave now!\n')
+                else:
+                    print("You can't catch this train anymore, sorry :(\n")
+
+        except KeyError:
+            continue
+    print('---------------------------------------------------------')
+
+while True:
+    trainDepature()
+    time.sleep(60)
